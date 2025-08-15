e0abc31522e5cfc4dfc1e8f122ef7faa24b4bf79
antonio.santese <antonio.santese@cetma.it>
2025-08-15 19:03:13 +0200

    from pepe to tomie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Pepe3D from "./components/Pepe3D/Pepe3D";
+import Tomie3D from "./components/Tomie3D/Tomie3D";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
-      <Pepe3D />
+      <Tomie3D />
     </div>
   );
 }
