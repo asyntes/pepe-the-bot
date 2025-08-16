0f34e1bfe6f451526d26153405b88d8bb9398f96
antonio.santese <antonio.santese@cetma.it>
2025-08-16 16:06:00 +0200

    fix opacity

--- src/app/components/TomieTerminal/TomieTerminal.tsx
+++ src/app/components/TomieTerminal/TomieTerminal.tsx
@@ -627,7 +627,7 @@ export default function TomieTerminal() {
                         style={{
                             transition: 'none',
                             transform: 'translateZ(0)', // Force hardware acceleration
-                            filter: `brightness(0) saturate(100%) ${currentMood === 'neutral' ? 'invert(47%) sepia(89%) saturate(2718%) hue-rotate(188deg) brightness(99%) contrast(101%)' :
+                            filter: `${currentMood === 'neutral' ? 'invert(47%) sepia(89%) saturate(2718%) hue-rotate(188deg) brightness(99%) contrast(101%)' :
                                 currentMood === 'angry' ? 'invert(23%) sepia(89%) saturate(6151%) hue-rotate(354deg) brightness(99%) contrast(107%)' :
                                     currentMood === 'trusted' ? 'invert(52%) sepia(98%) saturate(4466%) hue-rotate(269deg) brightness(96%) contrast(106%)' :
                                         currentMood === 'excited' ? 'invert(63%) sepia(99%) saturate(1174%) hue-rotate(15deg) brightness(103%) contrast(107%)' :
